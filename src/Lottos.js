989f0e6b0dd226be6635fd84eea158e581d18d41
aaapark <parksc1534.dev@gmail.com>
2023-11-05 22:34:48 +0900

    printRank

--- src/Lottos.js
+++ src/Lottos.js
@@ -49,18 +49,27 @@ export default class Lottos {
       }
 
     printRank(lottoRanks) {
+            
+        const WINNING_MESSAGE= [
+            '6개 일치 (2,000,000,000원)',
+            '5개 일치, 보너스 볼 일치 (30,000,000원)',
+            '5개 일치 (1,500,000원)',
+            '4개 일치 (50,000원)',
+            '3개 일치 (5,000원)',
+        ]
+
         const prize = [1,2,3,4,5]
         let newLottoRanks = [];
 
-        prize.forEach((e) => {
-            let count = lottoRanks.filter(i => i === e).length
-            newLottoRanks.push(count)
+        prize.forEach((e,index) => {
+            const count = lottoRanks.filter(i => i === e).length
+            const mesage = WINNING_MESSAGE[index]
+            newLottoRanks.push(`${mesage} - ${count}개`)
+        })
+
+        newLottoRanks.forEach((rank) => {
+            MissionUtils.Console.pirnt(rank)
         })
         
-        MissionUtils.Console.print(`3개 일치 (5,000원) - ${newLottoRanks[4]}개`)
-        MissionUtils.Console.print(`4개 일치 (50,000원) - ${newLottoRanks[3]}개`,)
-        MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${newLottoRanks[2]}개`,)
-        MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${newLottoRanks[1]}개`,)
-        MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${newLottoRanks[0]}개`,)
-    }
+    }     
 }
--- src/Score.js
+++ src/Score.js
@@ -1,19 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const WINNING_MESSAGE= [
+    '6개 일치 (2,000,000,000원)',
+    '5개 일치, 보너스 볼 일치 (30,000,000원)',
+    '5개 일치 (1,500,000원)',
+    '4개 일치 (50,000원)',
+    '3개 일치 (5,000원)',
+]
 function Test () {
     const prize = [1,2,3,4,5]
     const lottoRanks = [1,1,1,4,5,6,7,8]
     let newLottoRanks = [];
-    prize.forEach((e) => {
-        let count = lottoRanks.filter(i => i === e).length
-        newLottoRanks.push(count)
-    })
 
-    MissionUtils.Console.print(`3개 일치 (5,000원) - ${newLottoRanks[4]}개`)
-    MissionUtils.Console.print(`4개 일치 (50,000원) - ${newLottoRanks[3]}개`,)
-    MissionUtils.Console.print(`5개 일치 (1,500,000원) - ${newLottoRanks[2]}개`,)
-    MissionUtils.Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${newLottoRanks[1]}개`,)
-    MissionUtils.Console.print(`6개 일치 (2,000,000,000원) - ${newLottoRanks[0]}개`,)
+    prize.forEach((e,index) => {
+        const count = lottoRanks.filter(i => i === e).length
+        const mesage = WINNING_MESSAGE[index]
+        newLottoRanks.push(`${mesage} - ${count}개`)
+    })
+    newLottoRanks.forEach((rank) => {
+        MissionUtils.Console.pirnt(rank)
+    })
+    
 }
 
 Test()

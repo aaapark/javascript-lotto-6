4abdcf66ca88a5cd4efc2c83d7aa57a53180ce9d
aaapark <parksc1534.dev@gmail.com>
2023-11-07 00:59:30 +0900

    테스트코드 작업

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -1,22 +1,31 @@
-// import Bonus from "../src/BonusNumber.js";
+import Bonus from "../src/BonusNumber.js";
 
-// describe("보너스 클래스 테스트", () => {
-//   test("보너스 번호가 숫자가 아니면 예외가 발생한다.", () => {
-//     expect(() => {
-//       new Bonus(['bonus']);
-//     }).toThrow("[ERROR]");
-//   });
-// //
-//   test("보너스 번호가 당첨번호에 포함되어 있으면 예외가 발생한다.", (bonusNumber, winningNumber) => {
-//     const winningNumber = [12,23,34,35,41,45]
-//     expect(() => {
-//       new Bonus(bonusNumber,winningNumber);
-//     }).toThrow("[ERROR]");
-//   });
+describe("보너스 클래스 테스트", () => {
+  test("보너스 번호가 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Bonus(['bonus']);
+    }).toThrow("[ERROR]");
+  });
 
-//   test("보너스 번호가 1-45 사이의 숫자가 아니면 예외가 발생한다.", () => {
-//     expect(() => {
-//       new Bonus([98]);
-//     }).toThrow("[ERROR]");
-//   });  
-// });
+
+  test("보너스 번호가 당첨번호에 포함되어 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Bonus(23,[12,23,34,35,41,45]);
+    }).toThrow("[ERROR]");
+  });
+
+
+  test("보너스 번호가 1-45 사이의 숫자가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Bonus(498,[12,23,34,35,41,45]);
+    }).toThrow("[ERROR]");
+  })
+
+  test("보너스 번호는 당첨번호에 포함되지 않은 1-45 사이의 숫자이다", () => {
+    expect(() => {
+      new Bonus(12,[12,23,34,35,41,45]);
+    }).toBe({value: 12});
+
+
+  });  
+})
---
+++ __tests__/StringTest.js
@@ -0,0 +1,11 @@
+import App from '../src/App.js'
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+describe('문자열 테스트', () => {
+     test("split 매서드로 주어진 값을 구분.", () => {
+         expect(() => {
+            const app = new App()
+            app.userInputLottoNumber([1,2,3,4,5,6])
+        }).toContain(1,2,3,4,5,6);
+      });
+})
